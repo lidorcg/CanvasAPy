7eeda36c17fcc62b87cb30c75c861b20e609a0c2
lidorcg <lidorcg@gmail.com>
2015-10-29 21:16:32 +0200

    - added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='CanvasAPy',
+    version='',
+    packages=['CanvasAPy', 'CanvasAPy.OAuth', 'CanvasAPy.models'],
+    url='',
+    license='',
+    author='lidorcg',
+    author_email='',
+    description=''
+)
